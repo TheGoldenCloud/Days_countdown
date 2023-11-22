1db14201ab5fbb9b4d1fc38dc7818492db8942a4
GoldenCloudPc <jvladimir54@yahoo.com>
2023-11-22 16:17:41 +0100

    Update

--- electron/menubar.ts
+++ electron/menubar.ts
@@ -1,142 +1,142 @@
-import { Menu } from "electron";
+// import { Menu } from "electron";
 
-const { app, Menu,BrowserView } = require('electron')
+// const { app, Menu,BrowserView } = require('electron')
 
-// const isMac = process.platform === 'darwin'
+// // const isMac = process.platform === 'darwin'
 
-// const template: Electron.MenuItemConstructorOptions[] = [
-//   // { role: 'appMenu' }
-//   ...(isMac
-//     ? [{
-//         label: app.name,
-//         submenu: [
-//           { role: 'about' },
-//           // { type: 'separator' },
-//           // { role: 'services' },
-//           // { type: 'separator' },
-//           // { role: 'hide' },
-//           // { role: 'hideOthers' },
-//           // { role: 'unhide' },
-//           // { type: 'separator' },
-//           { role: 'quit' }
-//         ]
-//       }]
-//     : []),
-//   // { role: 'fileMenu' }
-//   // {
-//   //   label: 'File',
-//   //   submenu: [
-//   //     isMac ? { role: 'close' } : { role: 'quit' }
-//   //   ]
-//   // },
-//   // { role: 'editMenu' }
-//   // {
-//   //   label: 'Edit',
-//   //   submenu: [
-//   //     { role: 'undo' },
-//   //     { role: 'redo' },
-//   //     { type: 'separator' },
-//   //     { role: 'cut' },
-//   //     { role: 'copy' },
-//   //     { role: 'paste' },
-//   //     ...(isMac
-//   //       ? [
-//   //           { role: 'pasteAndMatchStyle' },
-//   //           { role: 'delete' },
-//   //           { role: 'selectAll' },
-//   //           { type: 'separator' },
-//   //           {
-//   //             label: 'Speech',
-//   //             submenu: [
-//   //               { role: 'startSpeaking' },
-//   //               { role: 'stopSpeaking' }
-//   //             ]
-//   //           }
-//   //         ]
-//   //       : [
-//   //           { role: 'delete' },
-//   //           { type: 'separator' },
-//   //           { role: 'selectAll' }
-//   //         ])
-//   //   ]
-//   // },
-//   // { role: 'viewMenu' }
-//   // {
-//   //   label: 'View',
-//   //   submenu: [
-//   //     { role: 'reload' },
-//   //     { role: 'forceReload' },
-//   //     { role: 'toggleDevTools' },
-//   //     { type: 'separator' },
-//   //     { role: 'resetZoom' },
-//   //     { role: 'zoomIn' },
-//   //     { role: 'zoomOut' },
-//   //     { type: 'separator' },
-//   //     { role: 'togglefullscreen' }
-//   //   ]
-//   // },
-//   // { role: 'windowMenu' }
-//   {
-//     label: 'Window',
-//     submenu: [
-//       { role: 'minimize' },
-//       { role: 'quit' },
-//       // { role: 'zoom' },
-//       // ...(isMac
-//       //   ? [
-//       //       { type: 'separator' },
-//       //       { role: 'front' },
-//       //       { type: 'separator' },
-//       //       { role: 'window' }
-//       //     ]
-//       //   : [
-//       //       { role: 'close' }
-//       //     ])
-//     ]
-//   },
-//   // {
-//   //   role: 'help',
-//   //   submenu: [
-//   //     {
-//   //       label: 'Learn More',
-//   //       click: async () => {
-//   //         const { shell } = require('electron')
-//   //         await shell.openExternal('https://electronjs.org')    //Postavi ovde guide
-//   //       }
-//   //     }
-//   //   ]
-//   // }
-// ]
-
-
-// module.exports = Menu.buildFromTemplate(template);
-
-// // 
+// // const template: Electron.MenuItemConstructorOptions[] = [
+// //   // { role: 'appMenu' }
+// //   ...(isMac
+// //     ? [{
+// //         label: app.name,
+// //         submenu: [
+// //           { role: 'about' },
+// //           // { type: 'separator' },
+// //           // { role: 'services' },
+// //           // { type: 'separator' },
+// //           // { role: 'hide' },
+// //           // { role: 'hideOthers' },
+// //           // { role: 'unhide' },
+// //           // { type: 'separator' },
+// //           { role: 'quit' }
+// //         ]
+// //       }]
+// //     : []),
+// //   // { role: 'fileMenu' }
+// //   // {
+// //   //   label: 'File',
+// //   //   submenu: [
+// //   //     isMac ? { role: 'close' } : { role: 'quit' }
+// //   //   ]
+// //   // },
+// //   // { role: 'editMenu' }
+// //   // {
+// //   //   label: 'Edit',
+// //   //   submenu: [
+// //   //     { role: 'undo' },
+// //   //     { role: 'redo' },
+// //   //     { type: 'separator' },
+// //   //     { role: 'cut' },
+// //   //     { role: 'copy' },
+// //   //     { role: 'paste' },
+// //   //     ...(isMac
+// //   //       ? [
+// //   //           { role: 'pasteAndMatchStyle' },
+// //   //           { role: 'delete' },
+// //   //           { role: 'selectAll' },
+// //   //           { type: 'separator' },
+// //   //           {
+// //   //             label: 'Speech',
+// //   //             submenu: [
+// //   //               { role: 'startSpeaking' },
+// //   //               { role: 'stopSpeaking' }
+// //   //             ]
+// //   //           }
+// //   //         ]
+// //   //       : [
+// //   //           { role: 'delete' },
+// //   //           { type: 'separator' },
+// //   //           { role: 'selectAll' }
+// //   //         ])
+// //   //   ]
+// //   // },
+// //   // { role: 'viewMenu' }
+// //   // {
+// //   //   label: 'View',
+// //   //   submenu: [
+// //   //     { role: 'reload' },
+// //   //     { role: 'forceReload' },
+// //   //     { role: 'toggleDevTools' },
+// //   //     { type: 'separator' },
+// //   //     { role: 'resetZoom' },
+// //   //     { role: 'zoomIn' },
+// //   //     { role: 'zoomOut' },
+// //   //     { type: 'separator' },
+// //   //     { role: 'togglefullscreen' }
+// //   //   ]
+// //   // },
+// //   // { role: 'windowMenu' }
+// //   {
+// //     label: 'Window',
+// //     submenu: [
+// //       { role: 'minimize' },
+// //       { role: 'quit' },
+// //       // { role: 'zoom' },
+// //       // ...(isMac
+// //       //   ? [
+// //       //       { type: 'separator' },
+// //       //       { role: 'front' },
+// //       //       { type: 'separator' },
+// //       //       { role: 'window' }
+// //       //     ]
+// //       //   : [
+// //       //       { role: 'close' }
+// //       //     ])
+// //     ]
+// //   },
+// //   // {
+// //   //   role: 'help',
+// //   //   submenu: [
+// //   //     {
+// //   //       label: 'Learn More',
+// //   //       click: async () => {
+// //   //         const { shell } = require('electron')
+// //   //         await shell.openExternal('https://electronjs.org')    //Postavi ovde guide
+// //   //       }
+// //   //     }
+// //   //   ]
+// //   // }
+// // ]
 
 
-const template: Menu = [{
-    label: 'Edit',
-    submenu: [
-        { role: 'undo' },
-        { role: 'redo' },
-    ]
-},
-{
-    label: 'View',
-    submenu: [
-        { role: 'reload' },
-    ]
-},
-{ role: 'window', submenu: [{ role: 'minimize' }, { role: 'close' }] },
-{
-    role: 'help',
-    submenu: [{
-        label: 'Learn More',
-        click() {
-            require('electron').shell.openExternal('https://electron.atom.io');
-        }
-    }]
-}
-];
+// // module.exports = Menu.buildFromTemplate(template);
 
-export default template;
+// // // 
+
+
+// // const template: Menu = [{
+// //     label: 'Edit',
+// //     submenu: [
+// //         { role: 'undo' },
+// //         { role: 'redo' },
+// //     ]
+// // },
+// // {
+// //     label: 'View',
+// //     submenu: [
+// //         { role: 'reload' },
+// //     ]
+// // },
+// // { role: 'window', submenu: [{ role: 'minimize' }, { role: 'close' }] },
+// // {
+// //     role: 'help',
+// //     submenu: [{
+// //         label: 'Learn More',
+// //         click() {
+// //             require('electron').shell.openExternal('https://electron.atom.io');
+// //         }
+// //     }]
+// // }
+// // ];
+
+// export default template;
--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import '../App.css'
+//import '../App.css'
+import Input from "./Input"
+import 'bootswatch/dist/solar/bootstrap.min.css'
 
 function App() {
 
   return (
     <>
-      
+      <Input/>
     </>
   )
 }
