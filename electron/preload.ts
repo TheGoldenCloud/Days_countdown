e2797764716bc54d07d85e99f72f2e8a4e24c978
GoldenCloudPc <jvladimir54@yahoo.com>
2023-11-24 23:42:18 +0100

    Exposed ipcrenderer obejct ro renderer process

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,36 +1,26 @@
 import { contextBridge, ipcRenderer } from 'electron'
 
-// --------- Expose some API to the Renderer process ---------
-contextBridge.exposeInMainWorld('ipcRenderer', withPrototype(ipcRenderer))
-
-contextBridge.exposeInMainWorld('electron', {
-  ipcRenderer: ipcRenderer,
-});
-
-
-//
-contextBridge.exposeInMainWorld('extensionFunc',{
-  myFunction: ()=>console.log("Hello worlddd")
+contextBridge.exposeInMainWorld('rendererFunctions', {
+  ipcRenderer: ipcRenderer
 })
 
-// `exposeInMainWorld` can't detect attributes and methods of `prototype`, manually patching it.
-function withPrototype(obj: Record<string, any>) {
-  const protos = Object.getPrototypeOf(obj)
+// function withPrototype(obj: Record<string, any>) {
+//   const protos = Object.getPrototypeOf(obj)
 
-  for (const [key, value] of Object.entries(protos)) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) continue
+//   for (const [key, value] of Object.entries(protos)) {
+//     if (Object.prototype.hasOwnProperty.call(obj, key)) continue
 
-    if (typeof value === 'function') {
-      // Some native APIs, like `NodeJS.EventEmitter['on']`, don't work in the Renderer process. Wrapping them into a function.
-      obj[key] = function (...args: any) {
-        return value.call(obj, ...args)
-      }
-    } else {
-      obj[key] = value
-    }
-  }
-  return obj
-}
+//     if (typeof value === 'function') {
+//       // Some native APIs, like `NodeJS.EventEmitter['on']`, don't work in the Renderer process. Wrapping them into a function.
+//       obj[key] = function (...args: any) {
+//         return value.call(obj, ...args)
+//       }
+//     } else {
+//       obj[key] = value
+//     }
+//   }
+//   return obj
+// }
 
 // --------- Preload scripts loading ---------
 function domReady(condition: DocumentReadyState[] = ['complete', 'interactive']) {
